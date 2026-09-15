785394427659941f3cf32c170e60d55f8fd8b01d
karthic <jkarthickeyan24@gmail.com>
2026-09-15 12:33:13 +0530

    chore: migrate from clsx and tailwind-merge to shadcn cn package

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,2 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
 /** Merge Tailwind classes (shadcn convention). */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export { cn } from "cn";
